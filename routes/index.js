91d11c5189351f88cdfe32bb09426a161e04eaec
Ryan <coryan25@gmail.com>
2017-06-08 13:27:05 +0000

    leave required data, eliminate the rest

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ router.get('/', function(req, res, next) {
 router.get('/api/whoami/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
     'use strict';
-    var ipv4 = address.ip();
-    var ipaddress = req.ip;
+    var ipaddress = address.ip();
+    var ipV6address = req.ip;
     var language = req.acceptsLanguages()[0];
     // var software = req.get('User-Agent');
     var software = useragent.parse(req.headers['user-agent']).os.family;
@@ -34,12 +34,12 @@ router.get('/api/whoami/', function(req, res, next) {
     res.json({
         ipaddress: ipaddress,
         language: language,
-        software: software,
+        software: software//,
         //UA: UA//,
-        ip2: ip2,
-        ip: ip,
-        host: host,
-        ipv4: ipv4
+        // ip2: ip2,
+        // ip: ip,
+        // host: host,
+        // ipV6address: ipV6address
         // userAgent: userAgent
     });
 });
